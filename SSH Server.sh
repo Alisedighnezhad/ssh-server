6d6b2d83b28dc4e2f4bc498e1a83f5e0ef6bc872
Alisn <alisedighnezhad@outlook.com>
2024-05-02 01:49:20 +0330

    Update SSH Server.sh

--- SSH Server.sh
+++ SSH Server.sh
@@ -22,17 +22,17 @@ userid() {
 
 # Making users 
 u=0
-while [ $u -lt 10 ]; do
-        id=$(userid) 
-        pass=$(pass)
-        echo "$id created with $pass password" >> log.txt  # Append to log.txt instead of overwriting
-        sudo useradd -m -g VPNs $id && echo "$id:$pass" 
-        u=$((u + 1))
+while [ $u -lt 2 ]; do
+  id=$(userid) 
+  pass=$(pass)
+  echo "$id created with $pass password" >> log.txt  # Append to log.txt instead of overwriting
+  sudo useradd -m -g VPNs $id 
+  echo -e "$pass\n$pass" | sudo passwd $id
+  echo "$pass"
+  u=$((u + 1))
 done
-sudo groupadd VPNs
 sudo chown -R ali:ali /home
 sudo chmod -R 770 /home
-sudo chmod -R 770 /etc
 sudo chgrp -R VPNs /home
 
 # just for Cheack Data
